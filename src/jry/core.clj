94ab5494bc9e550f2af2b2e62ffc26df626d8633
Jay Fields <github@jayfields.com>
2012-02-27 16:17:15 -0500

    fns from zubron

--- src/jry/core.clj
+++ src/jry/core.clj
@@ -1,3 +1,24 @@
-(ns jry.core)
+(ns jry.core
+  (:require clojure.data))
 
 (defn % [& args] (apply partial args))
+
+(defn truthy? [x]
+  (if x true false))
+
+(def falsey? (complement truthy?))
+
+(defn every [pred l]
+  (when (every? pred l)
+    l))
+
+(defn submap [sub super]
+  (->
+    (clojure.data/diff sub super)
+    last
+    (= sub)))
+
+(defmacro returning [[the-symbol v] & forms]
+  `(let [~the-symbol ~v]
+     ~@forms
+     ~the-symbol))
--- test/jry/test/core.clj
+++ test/jry/test/core.clj
@@ -3,3 +3,24 @@
 
 (expect fn? (% identity 1))
 (expect 1 ((% identity 1)))
+
+(expect 1 (returning [x 1] (* 2 x) (* 4 x)))
+
+(expect true (truthy? "BRN"))
+(expect true (truthy? true))
+(expect true (truthy? 1))
+(expect false (truthy? nil))
+(expect false (truthy? false))
+
+(expect false (falsey? "BRN"))
+(expect false (falsey? true))
+(expect false (falsey? 1))
+(expect true (falsey? nil))
+(expect true (falsey? false))
+
+(expect [1 2 3] (every identity [1 2 3]))
+(expect nil (every identity [1 2 nil]))
+
+(expect true? (submap {:a 1 :b {:c 2}} {:a 1 :b {:c 2 :d 3} :e 4}))
+(expect false? (submap {:a 1 :b {:c 2}} {:a "9" :b {:c 2 :d 3} :e 4}))
+(expect false? (submap {:a 1 :b {:c 2}} {:a 1 :b {:c "9" :d 3} :e 4}))
