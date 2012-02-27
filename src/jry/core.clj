e9bfe4108d6a538c5a772c0a213724c0ca9c95be
Jay Fields <github@jayfields.com>
2012-02-27 17:14:45 -0500

    b.i.m fns

--- src/jry/core.clj
+++ src/jry/core.clj
@@ -22,3 +22,56 @@
   `(let [~the-symbol ~v]
      ~@forms
      ~the-symbol))
+
+(defmacro ^{:private true} assert-args [fnname & pairs]
+  `(do (when-not ~(first pairs)
+    (throw (IllegalArgumentException.
+      ~(str fnname " requires " (second pairs)))))
+    ~(let [more (nnext pairs)]
+      (when more
+        (list* `assert-args fnname more)))))
+
+(defmacro obj->map [o & bindings]
+  (assert-args obj->map
+    (even? (count bindings)) "an even number of forms in binding vector")
+  (let [s (gensym "local")]
+    `(let [~s ~o]
+      ~(->> (apply hash-map bindings)
+        (map (fn [[k v]] [k (list v s)]))
+        (into {})))))
+
+(defn nth-vals* [a i m]
+  (if (and (map? m) (> i 0))
+    (reduce into a (map (fn [v] (nth-vals* a (dec i) v)) (vals m)))
+    (conj a m)))
+
+(defn nth-vals [i m]
+  (if (nil? m)
+    {}
+    (nth-vals* [] i m)))
+
+(defn flatten-keys* [a ks m]
+  (if (map? m)
+    (reduce into {} (map (fn [[k v]] (flatten-keys* a (conj ks k) v)) (seq m)))
+    (assoc a ks m)))
+
+(defn flatten-keys [m]
+  (if (nil? m)
+    {}
+    (flatten-keys* {} [] m)))
+
+(defn nth-vals* [a i m]
+  (if (and (map? m) (> i 0))
+    (reduce into a (map (fn [v] (nth-vals* a (dec i) v)) (vals m)))
+    (conj a m)))
+
+(defn nth-vals [i m]
+  (if (nil? m)
+    {}
+    (nth-vals* [] i m)))
+
+(defn update-values [m f & args]
+  (reduce (fn [r [k v]] (assoc r k (apply f v args))) {} m))
+
+(defn key-by [f coll]
+  (reduce (fn [r e] (assoc r (f e) e)) {} coll))
--- test/jry/test/core.clj
+++ test/jry/test/core.clj
@@ -24,3 +24,27 @@
 (expect true? (submap {:a 1 :b {:c 2}} {:a 1 :b {:c 2 :d 3} :e 4}))
 (expect false? (submap {:a 1 :b {:c 2}} {:a "9" :b {:c 2 :d 3} :e 4}))
 (expect false? (submap {:a 1 :b {:c 2}} {:a 1 :b {:c "9" :d 3} :e 4}))
+
+(expect {:month 0 :year 2012 :day 31}
+  (obj->map (java.util.Date. 2012 0 31)
+    :month .getMonth
+    :year .getYear
+    :day .getDate))
+
+(expect {[:a :b] 1 [:a :e] 3 [:c :d] 2} (flatten-keys {:a {:b 1 :e 3} :c {:d 2}}))
+(expect {[:a :z] nil [:a :b] 1 [:a :e :f] 4 [:a :e :g] 5}
+  (flatten-keys {:a {:z nil :b 1 :e {:f 4 :g 5}}}))
+(expect {} (flatten-keys {}))
+(expect {} (flatten-keys nil))
+
+(expect {:b {:e :f} :h {:e :f}}
+  (update-values {:b {:c :d :e :f} :h {:c :d :e :f}} dissoc :c))
+
+(expect [3 6]
+  (nth-vals 2 {1 {2 3} 4 {5 6}}))
+
+(expect [3 {6 7} :a]
+  (nth-vals 2 {:a :a 1 {2 3} 4 {5 {6 7}}}))
+
+(expect {1 {:a 1 :b 1} 2 {:a 2 :b 2} 3 {:a 3 :b 3}}
+  (key-by :a [{:a 1 :b 1} {:a 2 :b 2} {:a 3 :b 3}]))
