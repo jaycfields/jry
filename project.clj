98ae66f192a11d8e2c3f60ac4164171a754c7965
Jay Fields <github@jayfields.com>
2012-02-28 09:13:17 -0500

    remove package.fig and use lein-publishers

--- project.clj
+++ project.clj
@@ -3,25 +3,5 @@
   :jar-name "jry.jar"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[expectations "1.3.5"]
-                     [lein-expectations "0.0.1"]])
-
-(ns leiningen.publish-fig
-  (:require leiningen.jar)
-  (:use clojure.java.shell))
-
-(defn publish-fig [project & args]
-  (leiningen.jar/jar project)
-  (let [response (apply sh "fig" "--publish" (str (:name project) "/" (:version project)) args)]
-    (println "OUT:" (:out response))
-    (println "ERR:" (:err response))))
-
-(ns leiningen.publish-clojars
-  (:require leiningen.jar)
-  (:use clojure.java.shell))
-
-(defn publish-clojars [project & args]
-  (leiningen.jar/jar project)
-  (leiningen.pom/pom project)
-  (let [response (sh "scp" "pom.xml" "jry.jar" "clojars@clojars.org:")]
-    (println "OUT:" (:out response))
-    (println "ERR:" (:err response))))
+                     [lein-expectations "0.0.1"]
+                     [lein-publishers "1.0.4"]])
