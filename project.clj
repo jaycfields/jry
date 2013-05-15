0cec912ce75f734c89b83d0c7fd252da447daf56
Jay Fields <jay@jayfields.com>
2013-05-15 14:55:41 -0400

    upgrade lein-publishers

--- project.clj
+++ project.clj
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :profiles {:dev {:dependencies [[expectations "1.4.36"]]}}
   :plugins [[lein-expectations "0.0.7"]
-            [lein-publishers "1.0.4"]])
+            [lein-publishers "1.0.10"]])
