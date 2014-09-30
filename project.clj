54b486de52e63c46c0ac5bc6fa6f260d5142867b
Jay Fields <jay@jayfields.com>
2014-09-30 09:49:20 -0400

    update project and add file related fns

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject jry "3.0.0"
+(defproject jry "5.1.0"
   :description "general clojure functions"
   :jar-name "jry.jar"
   :source-paths ["src/clojure"]
   :test-paths ["test"]
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :deploy-repositories [["releases" :clojars]]
   :profiles {:dev {:dependencies [[expectations "1.4.36"]]}}
   :plugins [[lein-expectations "0.0.7"]
-            [lein-publishers "1.0.10"]])
+            [lein-publishers "1.0.11"]])
