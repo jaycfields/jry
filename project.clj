5be32b63726776b48cb952c1b0b446d168e86f75
Jay Fields <jay@jayfields.com>
2014-11-19 06:36:15 -0500

    Version 5.1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jry "5.1.8-SNAPSHOT"
+(defproject jry "5.1.8"
   :description "general clojure functions"
   :url "https://github.com/jaycfields/jry/blob/master/README.md"
   :license "https://github.com/jaycfields/jry/blob/master/README.md#license"
